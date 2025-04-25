48d0a1ecfb3346e4034713fcd5b9cb1387b64cfc
Benedikt Schäfer <benediktschaefer@protonmail.com>
2025-04-25 15:32:12 +0200

    hotfix for buggy generator

--- oapigen.ps1
+++ oapigen.ps1
@@ -5,11 +5,27 @@ if (-Not (Test-Path -Path "oapi-generator" -PathType Container)) {
     New-Item -ItemType Directory -Path "oapi-generator" -Force | Out-Null
     Set-Location -Path "oapi-generator"
     
-    # Download the openapi-generator-cli script
     & Invoke-WebRequest -OutFile openapi-generator-cli.jar https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/7.12.0/openapi-generator-cli-7.12.0.jar
+    & Invoke-WebRequest -OutFile openapi.yml raw.githubusercontent.com/Chrystalkey/landtagszusammenfasser/refs/heads/dev-specchange/docs/specs/openapi.yml
+    Set-Location -Path ".."
+}
+if (-Not (Test-Path -Path "oapi-generator/openapi-generator-cli.jar" -PathType Leaf)) {
+    Write-Host "Downloading OApi Generator"
+    Set-Location -Path "oapi-generator"
+    & Invoke-WebRequest -OutFile openapi-generator-cli.jar https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/7.12.0/openapi-generator-cli-7.12.0.jar
+    Set-Location -Path ".."
+}
+if (-Not (Test-Path -Path "oapi-generator/openapi.yml" -PathType Leaf)) {
+    Write-Host "Downloading OApi Spec"
+    Set-Location -Path "oapi-generator"
     & Invoke-WebRequest -OutFile openapi.yml raw.githubusercontent.com/Chrystalkey/landtagszusammenfasser/refs/heads/dev-specchange/docs/specs/openapi.yml
     Set-Location -Path ".."
 }
 
 
 & java -jar "./oapi-generator/openapi-generator-cli.jar" generate -g rust-axum -i "$(Get-Location)/oapi-generator/openapi.yml" -o "$(Get-Location)/oapicode"
+& rg "<I, A, E>" -r "<I, A, E, C>" .\oapicode\src\server\mod.rs --passthrough -N > .\oapicode\src\server\mod1.rs
+Get-Content .\oapicode\src\server\mod1.rs | Set-Content -Encoding utf8 .\oapicode\src\server\mod2.rs
+& rm .\oapicode\src\server\mod.rs
+& rm .\oapicode\src\server\mod1.rs
+& mv .\oapicode\src\server\mod2.rs .\oapicode\src\server\mod.rs
--- oapigen.sh
+++ oapigen.sh
@@ -1,18 +1,48 @@
+#!/bin/bash
+
 export OPENAPI_GENERATOR_VERSION="7.12.0"
 DIRECTORY="oapi-generator"
 
 echo "Checking for openapi-generator-cli"
 
-if [ -d "$DIRECTORY" ]; then
-    echo "Directory $DIRECTORY found!"
-else
-    echo "Creating $DIRECTORY directory"
-    mkdir "$DIRECTORY"
-    cd "$DIRECTORY"
-    curl https://raw.githubusercontent.com/OpenAPITools/openapi-generator/master/bin/utils/openapi-generator-cli.sh > openapi-generator-cli
-    chmod u+x openapi-generator-cli
-    curl raw.githubusercontent.com/Chrystalkey/landtagszusammenfasser/refs/heads/dev-specchange/docs/specs/openapi.yml > openapi.yml
+# Create oapi-generator directory if it doesn't exist
+if [ ! -d "oapi-generator" ]; then
+    echo "Creating oapi-generator directory"
+    mkdir oapi-generator
+    cd oapi-generator || exit
+
+    curl -o openapi-generator-cli.jar https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/7.12.0/openapi-generator-cli-7.12.0.jar
+    curl -o openapi.yml https://raw.githubusercontent.com/Chrystalkey/landtagszusammenfasser/refs/heads/dev-specchange/docs/specs/openapi.yml
+
     cd ..
 fi
 
+# Download JAR if missing
+if [ ! -f "oapi-generator/openapi-generator-cli.jar" ]; then
+    echo "Downloading OApi Generator"
+    cd oapi-generator || exit
+    curl -o openapi-generator-cli.jar https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/7.12.0/openapi-generator-cli-7.12.0.jar
+    cd ..
+fi
+
+# Download YAML spec if missing
+if [ ! -f "oapi-generator/openapi.yml" ]; then
+    echo "Downloading OApi Spec"
+    cd oapi-generator || exit
+    curl -o openapi.yml https://raw.githubusercontent.com/Chrystalkey/landtagszusammenfasser/refs/heads/dev-specchange/docs/specs/openapi.yml
+    cd ..
+fi
+
+# Run openapi-generator
 OPENAPI_GENERATOR_VERSION="7.12.0" ./$DIRECTORY/openapi-generator-cli generate -g rust-axum -i $DIRECTORY/openapi.yml -o $(pwd)/oapicode
+
+# Replace <I, A, E> with <I, A, E, C> in mod.rs using ripgrep and create mod2.rs
+rg '<I, A, E>' -r '<I, A, E, C>' ./oapicode/src/server/mod.rs --passthrough -N > ./oapicode/src/server/mod1.rs
+
+# Convert to UTF-8 and move the file
+iconv -t UTF-8 ./oapicode/src/server/mod1.rs -o ./oapicode/src/server/mod2.rs
+
+# Replace mod.rs with mod2.rs
+rm ./oapicode/src/server/mod.rs
+rm ./oapicode/src/server/mod1.rs
+mv ./oapicode/src/server/mod2.rs ./oapicode/src/server/mod.rs
