b953061e89d84e3d74fff142d299db94dcabd09e
Benedikt Schäfer <benediktschaefer@protonmail.com>
2025-09-03 18:02:19 +0200

    migration

--- migrations/20250302145218_auth-rework.sql
+++ migrations/20250302145218_auth-rework.sql
@@ -1,1 +1,9 @@
 -- Add migration script here
+ALTER TABLE api_keys ADD COLUMN salt VARCHAR NOT NULL DEFAULT 'ltzf_defaultsalt';
+ALTER TABLE api_keys ADD COLUMN keytag VARCHAR UNIQUE DEFAULT NULL;
+
+UPDATE api_keys SET keytag = SUBSTRING(key_hash, 0, 16) WHERE keytag IS NULL;
+
+ALTER TABLE api_keys ALTER COLUMN keytag SET NOT NULL;
+
+ALTER TABLE api_keys DROP COLUMN coll_id;
